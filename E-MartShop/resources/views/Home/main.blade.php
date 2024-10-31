d587e6b36a9b3e8f735367213c813e6f81402ddf
YomarRivera <yriverabonet@student.cscc.edu>
2024-10-30 20:46:41 -0400

    Update main.blade.php

--- E-MartShop/resources/views/Home/main.blade.php
+++ E-MartShop/resources/views/Home/main.blade.php
@@ -7,10 +7,109 @@
     
   </head>
   <body>
+ <div id="wrapper">
+      <header>
+        <img src="Logo.png" alt="Games logo" height="70px" class="formatleft" /> 
 
+        <h1>Top Video Games of All Times</h1> 
 
+      </header>
+      <nav>
+        <ul id="navlist">
 
+          <li> <a href="1.html">Home</a></li> 
+          <li> <a href="2.html">Getting Started</a></li> 
+          <li><a href="3.html">Top Games</a></li> 
+          <li><a href="4.html">About</a></li> 
+        </ul>
+      </nav> 
+      <section>
 
+        <h2 class="A2022">The Most Popular Games of All Times</h2>
+        <p class="A2022">Ranked By Highest Player Count Of All Times </p>
 
+        <article class="hi4">
+        <img src="tetris.png" alt="Game" class="IMG"/>
+        <h2><a href="https://www.ign.com/articles/2011/12/23/tetris-review">Tetris</a></h2>
+
+        <p>#1, 520 million players</p>
+
+                </article>
+
+                <article class="hi4">
+                    <img src="minecraft.png" alt="Game" class="IMG"/>
+                    <h2><a href="https://www.ign.com/articles/2011/11/24/minecraft-review">Minecraft</a></h2>
+
+                    <p>#2, 300 million players</p>
+
+                </article>
+
+                <article class="hi4">
+                    <img src="GTA5.png" alt="Game" class="IMG"/>
+                    <h2><a href="https://www.ign.com/articles/2013/09/16/grand-theft-auto-v-review">Grand Theft Auto V</a></h2>
+
+                    <p>#3, 200 million+ players</p>
+
+                </article>
+
+                <article class="hi4">
+                    <img src="wii.jpeg" alt="Game" class="IMG"/>
+                    <h2><a href="https://www.ign.com/articles/2006/11/14/wii-sports-review">Wii Sports</a></h2>
+
+                    <p>#4, 82.9 million players</p>
+
+                </article>
+                
+                <article class="hi4">
+                    <img src="PUBG.jpeg" alt="Game" class="IMG"/>
+                    <h2><a href="https://www.ign.com/articles/2018/01/05/playerunknowns-battlegrounds-review">PUBG</a></h2>
+
+                    <p>#5, 75 million players</p>
+
+                </article>
+
+                <article class="hi4">
+                    <img src="mario.jpeg" alt="Game" class="IMG"/>
+                    <h2><a href="https://www.ign.com/articles/2017/04/20/mario-kart-8-deluxe-review">Mario Kart 8 Deluxe</a></h2>
+
+                    <p>#6, 70.43 million players</p>
+
+                </article>
+                <article class="hi4">
+                    <img src="RDR2.avif" alt="Game" class="IMG"/>
+                    <h2><a href="https://www.ign.com/articles/2018/10/25/red-dead-redemption-2-review">Red Dead Redemption 2</a></h2>
+
+                    <p>#7, 64 million players</p>
+
+                </article>
+
+                <article class="hi4">
+                    <img src="TESVS.png" alt="Game" class="IMG"/>
+                    <h2><a href="https://www.ign.com/articles/2011/11/10/the-elder-scrolls-v-skyrim-review">The Elder Scrolls V: Skyrim</a></h2>
+
+                    <p>#8, 60 million players</p>
+
+                </article>
+            
+                <article class="hi4">
+                    <img src="Terraria.jpeg" alt="Game" class="IMG"/>
+                    <h2><a href="https://www.ign.com/articles/2011/05/25/terraria-review">Terraria</a></h2>
+
+                    <p>#9, 58.7 million players</p>
+
+                </article>
+
+                <article class="hi4">
+                    <img src="Witcher.jpeg" alt="Game" class="IMG"/>
+                    <h2><a href="https://www.ign.com/articles/2015/05/12/the-witcher-3-the-wild-hunt-review">The Witcher 3: Wild Hunt</a></h2>
+
+                    <p>#10, 50 million+ players</p>
+
+                </article>
+            </section>
+            <footer>  
+                <p class="RL"><a href="https://www.ign.com/articles/best-selling-video-games-of-all-time-grand-theft-auto-minecraft-tetris">Reference website<a></p>
+            </footer>
+    </div>
   </body>
 </html>
